9951ac1821baab1991b9c4cafc9fdbeb2441904d
anhcuongsuper <cuongnld00635@fpt.edu.vn>
2018-11-02 23:09:48 +0700

    Ok

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ASP_NetMVC.Models;
+
+namespace ASP_NetMVC.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly MyDbContext context;
+      
+        public ProductController(MyDbContext context)
+           
+        {
+           
+            if (!context.Product.Any())
+            {
+                context.Product.Add(new Product()
+                {
+                    name = "Cuong",
+                    price = "100",
+                });
+                context.Product.Add(new Product()
+                {
+                    name = "Truc",
+                    price = "200;"
+                });
+                context.Product.Add(new Product()
+                {
+                    name = "Hang",
+                    price = "1200"
+                }
+                  );
+                context.SaveChanges();
+            }
+        }
+        [HttpGet]
+        public IActionResult Index(MyDbContext context)
+        {
+            return View(context.Product.ToList());
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        public IActionResult Edit(MyDbContext context, int Id)
+        {
+            var exitProduct = context.Product.Find(Id);
+            if (exitProduct == null)
+            {
+                return NotFound();
+            }
+            return View(exitProduct);
+        }
+        public IActionResult GetList()
+        {
+            return View(context.Product.ToList());
+        }
+        public IActionResult Update(MyDbContext context, Product product)
+        {
+            var exisProduct = context.Product.Find(product._id);
+            if (exisProduct == null)
+            {
+                return NotFound();
+            }
+            exisProduct.name = product.name;
+            exisProduct.price = product.price;
+            context.Product.Update(exisProduct);
+            context.SaveChanges();
+            TempData["message"] = "Update success";
+            return Redirect("Index");
+        }
+        [HttpPost]
+        public IActionResult Save(MyDbContext contex, Product product)
+        {
+            contex.Product.Add(product);
+            contex.SaveChanges();
+            TempData["message"] = "Insert success";
+            return Redirect("Index");
+           
+        }
+        [HttpDelete]
+        public IActionResult Delete(MyDbContext contex , int Id)
+        {
+            var product = context.Product.Find(Id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            contex.Product.Remove(product);
+            contex.SaveChanges();
+            return Json(product);
+        }
+        [HttpDelete]
+        public IActionResult DeleteMany(string ids)
+        {
+            return Json(ids);
+        }
+    }
+
+    
+
+}
---
+++ Models/MyDbContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP_NetMVC
+{
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext(DbContextOptions<MyDbContext> options)
+               : base(options)
+        {
+        }
+
+        public DbSet<Product> Product { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+    public class Category
+    {
+        public long id { get; set; }
+        public string Name { get; set; }
+    }
+    public class Product
+    {
+
+        public int _id { get; set; }
+        public string name { get; set; }
+        public string price { get; set; }
+        public string add { get; set; }
+        public string Save { get; set; }
+    }
+}
